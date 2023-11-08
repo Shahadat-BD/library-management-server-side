4d6737f66a2a772e4622b941eb6f9710b6f05d80
Shahadat-BD <shahadatstudent34@gmail.com>
2023-11-08 09:13:33 -0800

    jwt added for add-books route and all books route

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cookieParser = require('cookie-parser');
 require('dotenv').config()
 
 const port = process.env.PORT || 3000
 
 
-app.use(cors())
+app.use(cors({
+  origin : [
+    'http://localhost:5173',
+    "https://bookstack-auth-7442e.web.app/",
+    "https://bookstack-auth-7442e.firebaseapp.com/"
+  ],
+  credentials : true
+}))
 app.use(express.json())
-
-
+app.use(cookieParser())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lwsgehv.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -23,10 +31,34 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+// Middlewares 
+
+const logger = (req,res,next) => {
+  next()
+}
+
+const verifyToken = (req,res,next) =>{
+  const token = req?.cookies?.token
+ if (!token) {
+      return res.status(401).send({message : "unauthorized access"})
+ }
+ jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded) => {
+     if (err) {
+        return res.status(401).send({message : "unauthorized access"})
+     } 
+     req.user = decoded;
+     next()
+ })
+}
+
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const categoryCollection = client.db("BooksLibrary").collection('bookCategory');
     const booksCollection = client.db("BooksLibrary").collection('booksAdded');
@@ -39,15 +71,21 @@ async function run() {
           })
    
     // book added in database using post method
-       app.post('/books-add',async(req,res)=>{
+       app.post('/books-add',logger,verifyToken,async(req,res)=>{
+        if (!req.user.email) {
+          res.status(403).send({message : "forbidden access"})
+         }
            const books = req.body
            const result = await booksCollection.insertOne(books)
            res.send(result)
        })
       // all category books getting form database using get method
-         app.get('/books-add',async(req,res)=>{
+         app.get('/books-add',logger,verifyToken,async(req,res)=>{
+          if (!req.user.email) {
+            res.status(403).send({message : "forbidden access"})
+         }
           const result = await booksCollection.find().toArray()
-          res.send(result)
+          res.send(result) 
          })
 
         // books updated by put api method
@@ -85,14 +123,15 @@ async function run() {
           const query = {_id: new ObjectId(id)}
           const result = await booksCollection.findOne(query)
           res.send(result) 
-      })
-
+      }) 
+  
       //  specific Book for some core concept reading of this book.
         app.get('/readBook/:id',async(req,res)=>{
               const id = req.params.id
               const query = {_id: new ObjectId(id)}
               const result = await booksCollection.findOne(query)
               res.send(result)  
+
         })
 
         // borrowed books added in database using post api
@@ -103,8 +142,8 @@ async function run() {
         })
 
         // specific email based borrowed books collect 
-        app.get('/borrowd-books',async(req,res)=>{
-          let query = {}
+        app.get('/borrowd-books',async(req,res)=>{ 
+          let query = {} 
           if (req.query?.email) {
              query = { email : req.query.email } 
           }
@@ -137,6 +176,27 @@ async function run() {
             const result = await borrowedCollection.deleteOne(query);
             res.send(result)
           })
+
+          // auth related api 
+          app.post('/jwt',async(req,res) => {
+          const user = req.body;
+  
+          const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET , {expiresIn : '2h'})
+          
+          res.cookie('token',token,{
+            httpOnly : true,
+            secure : true,
+            sameSite : 'none'
+        })
+        .send({success : true})
+      })
+
+     // logout api => when user logout then token get out form cooke.
+         app.post('/logout',async(req,res) => {
+         const user  =  req.body;
+        res.clearCookie('token',{maxAge:0})
+        .send({success : true})
+})
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
